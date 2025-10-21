59f78c3dca0fe1590879aaceb0fe78539bd24ea9
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 03:49:31 -0400

    Refactor footer to use custom element SiteFooter

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,18 @@
-    <footer>
+// components/footer.js
+class SiteFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <footer>
         <div class="footer-content">
-            <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
-            <div class="social-links">
-                <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
-                <a href="https://substack.com/@breathinganomalypseudonym">Sub</a>
-            </div>
+          <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
+          <div class="social-links">
+            <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
+            <a href="#">Tw</a>
+            <a href="https://substack.com/@breathinganomalypseudonym">Sub</a>
+          </div>
         </div>
-    </footer>
+      </footer>
+    `;
+  }
+}
+customElements.define('site-footer', SiteFooter);
