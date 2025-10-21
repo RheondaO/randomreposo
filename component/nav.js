0cd9f301e62bec96f29f8d5ee39a4d5555e47ac7
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 12:18:01 -0400

    Update nav.js

--- component/nav.js
+++ component/nav.js
@@ -1,20 +1,12 @@
-// components/nav.js
-class SiteNav extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <nav>
-        <div class="nav-container">
-          <a href="index.html" class="logo">ai portfolio</a>
-          <ul class="nav-links">
-            <li><a href="index.html">home</a></li>
-            <li><a href="knowledgebase.html">knowledgebase</a></li>
-            <li><a href="agentlibrary.html">agent library</a></li>
-            <li><a href="thelab.html">the lab</a></li>
-            <li><a href="contact.html">contact</a></li>
-          </ul>
-        </div>
-      </nav>
-    `;
-  }
+// /component/nav.js
+export function loadNav() {
+  document.getElementById("nav").innerHTML = `
+    <nav>
+      <a href="/index.html">Home</a>
+      <a href="/knowledgebase.html">Knowledgebase</a>
+      <a href="/agentlibrary.html">Agent Library</a>
+      <a href="/thelab.html">The Lab</a>
+      <a href="/contact.html">Contact</a>
+    </nav>
+  `;
 }
-customElements.define('site-nav', SiteNav);
