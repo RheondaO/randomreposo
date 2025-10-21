02ce8105a8964ba59f988dcbd3d2815c19843b60
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 12:36:57 -0400

    Update footer.js

--- component/footer.js
+++ component/footer.js
@@ -1,18 +1,16 @@
-// components/footer.js
-class SiteFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <footer>
-        <div class="footer-content">
-          <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
-          <div class="social-links">
-            <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
-            <a href="#">Tw</a>
-            <a href="https://substack.com/@breathinganomalypseudonym">Sub</a>
-          </div>
-        </div>
-      </footer>
-    `;
-  }
+// /component/footer.js
+export function loadFooter() {
+  const footerContainer = document.getElementById("footer");
+  if (!footerContainer) return;
+
+  footerContainer.innerHTML = `
+    <div class="footer-content">
+      <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
+      <div class="social-links">
+        <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
+        <a href="#">Tw</a>
+        <a href="https://substack.com/@breathinganomalypseudonym">Sub</a>
+      </div>
+    </div>
+  `;
 }
-customElements.define('site-footer', SiteFooter);
