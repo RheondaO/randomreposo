3c4c11900190f9b8580280d9fb2a2be05d07e620
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 03:25:16 -0400

    Add footer component with copyright and social links

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+    <footer>
+        <div class="footer-content">
+            <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
+            <div class="social-links">
+                <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
+                <a href="https://substack.com/@breathinganomalypseudonym">Sub</a>
+            </div>
+        </div>
+    </footer>
