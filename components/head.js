548c182990241eb123aca98bb47237d1324a27c4
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 10:02:48 -0400

    Update head.js

--- components/head.js
+++ components/head.js
@@ -28,7 +28,7 @@ export function loadSharedHead({
 
   const favicon = document.createElement("link");
   favicon.rel = "icon";
-  favicon.href = "/folderder/favicon.png";
+  favicon.href = "folderder/favicon.png";
 
   // Fonts
   const fontLink = document.createElement("link");
