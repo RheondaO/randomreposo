008d55830e49ec48a297d99b69b456d71391e408
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-21 20:27:08 -0400

    Update footer.js

--- component/footer.js
+++ component/footer.js
@@ -8,7 +8,7 @@ export function loadFooter() {
       <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
       <div class="social-links">
         <a href="https://www.linkedin.com/in/rheondadogletree">Li</a>
-        <a href="#">Tw</a>
+        <a href="https://github.com/RheondaO">Git</a>
         <a href="https://breathinganomalypseudonym.substack.com/">Sub</a>
       </div>
     </div>
