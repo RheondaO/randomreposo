03b9064efa713ad25a1328596f2df52f2c8857c2
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2025-10-31 03:14:37 -0400

    Update head.js

--- component/head.js
+++ component/head.js
@@ -5,7 +5,7 @@ export function loadSharedHead({ title, description }) {
   const headContent = `
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="${description || "AI Portfolio showcasing Rheonda’s AI and automation projects."}" />
+    <meta name="description" content="${description || "AI Portfolio showcasing AI and automation projects."}" />
     
     <link rel="icon" type="image/png" href="./art/favicon.png" />
     <link rel="stylesheet" href="./art/style.css" />
@@ -14,14 +14,13 @@ export function loadSharedHead({ title, description }) {
   document.head.insertAdjacentHTML("beforeend", headContent);
 
   document.addEventListener("DOMContentLoaded", async () => {
-    const { loadLoader } = await import("./loader.js");
     const { loadNav } = await import("./nav.js");
     const { loadFooter } = await import("./footer.js");
-    
+    const { loadLoader } = await import("./loader.js");
 
-    loadLoader();
+    
     loadNav();
     loadFooter();
-    
+    loadLoader();
   });
 }
