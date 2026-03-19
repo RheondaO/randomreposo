92693e502cf0de52da4541a8d46e7566e40fa51a
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2026-03-19 19:43:30 -0400

    Update copyright year in footer content

--- component/footer.js
+++ component/footer.js
@@ -5,7 +5,7 @@ export function loadFooter() {
 
   footerContainer.innerHTML = `
     <div class="footer-content">
-      <p>&copy; 2025 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
+      <p>&copy; 2026 Property of Rheonda Dorothy Ogletree. Built with Transparency. All rights reserved.</p>
       <div class="social-links">
         <a href="https://github.com/RheondaO">Git</a>
         <a href="https://breathinganomalypseudonym.substack.com/">Sub</a>
