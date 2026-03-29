665dc732589a9048ab30fa863652ea5562d1ea06
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2026-03-28 23:39:43 -0400

    Update nav style

--- component/nav.js
+++ component/nav.js
@@ -8,15 +8,18 @@ export function loadNav() {
       <div class="nav-container">
         <a href="index.html" class="logo">sales portfolio</a>    
   <ul class="nav-links">
-          <li><a href="index.html">home</a></li>
-        <li><a href="#" class="disabled">meet the admin</a>
-        <span class="nav-tooltip">coming soon <3</span></li>
-          <li><a href="#" class="disabled">knowledgebase</a>
-          <span class="nav-tooltip">coming soon <3</span></li>
-          <!-- <li><a href="agentlibrary.html">agent library</a></li> -->
-          <!-- <li><a href="thelab.html">the lab</a></li> -->
-          <!-- <li><a href="contact.html">contact</a></li> -->
-        </ul>
+        <li><a href="index.html">home</a></li>
+
+        <li class="nav-item">
+          <a href="#" class="disabled">meet the admin</a>
+          <span class="nav-tooltip">coming soon <3</span>
+        </li>
+
+        <li class="nav-item">
+          <a href="#" class="disabled">knowledgebase</a>
+          <span class="nav-tooltip">coming soon <3</span>
+        </li>
+      </ul>
   </div>
       </div>
     </nav>
