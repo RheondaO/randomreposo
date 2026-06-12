4eb04ecaefc90313d598219c8657eece9160d75c
rheonda.do <35320005+RheondaO@users.noreply.github.com>
2026-06-12 01:40:22 -0400

    Enhance head.js with preconnect and font links
    Added preconnect links for Google APIs and fonts.

--- component/head.js
+++ component/head.js
@@ -7,6 +7,11 @@ export function loadSharedHead({ title, description }) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="${description || "Sales Portfolio showcasing Mutability"}" />
     
+    <link rel="preconnect" href="https://googleapis.com">
+    <link rel="preconnect" href="https://gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Jersey+25&family=Open+Sans:ital,wdth,wght@0,88.8,333;1,88.8,333&display=swap" rel="stylesheet">
+    
     <link rel="icon" type="image/png" href="./art/favicon.png" />
     <link rel="stylesheet" href="./art/style.css" />
   `;
